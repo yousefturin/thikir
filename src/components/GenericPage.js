f4fafa1adb253e17d9ca6b7798c032f3ce3da746
yousefturin <yusefturin@gmail.com>
2023-09-18 17:01:43 +0300

    added the button for the count and the function

--- src/components/GenericPage.js
+++ src/components/GenericPage.js
@@ -1,33 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Vibration, ScrollView } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Vibration,
+  ScrollView,
+  ImageBackground,
+  TouchableWithoutFeedback ,
+} from "react-native";
 
 const GenericPage = ({ route }) => {
   const { item } = route.params;
   const [currentIndex, setCurrentIndex] = useState(0);
   const [maxDescriptionHeight, setMaxDescriptionHeight] = useState(0);
+  const [count, setCount] = useState(0);
+  const ControlPaneBackgroundImage = require("../../assets/HeaderBackground.jpg");
 
-  
   useEffect(() => {
     // Find the maximum height based on the character length of subItemDescription
     let maxHeight = 150; // Default height for descriptions with less than or equal to 1000 characters
     const subItemDescription = item.subItems[currentIndex].subItemDescription;
-  
+
     // Remove non-printable characters and control characters
-    const sanitizedDescription = subItemDescription.replace(/[^ -~]+/g, '');
-  
-    console.log("sanitized subItemDescription length is:", sanitizedDescription.length);
-    
+    const sanitizedDescription = subItemDescription.replace(/[^ -~]+/g, "");
+
+    console.log(
+      "sanitized subItemDescription length is:",
+      sanitizedDescription.length
+    );
+
     if (sanitizedDescription.length > 30) {
       maxHeight = 450;
     }
-  
+
     setMaxDescriptionHeight(maxHeight);
   }, [item.subItems, currentIndex]);
+
+
+  useEffect(() => {
+    // Check if the count has reached the specified value
+    if (count >= item.subItems[currentIndex].count) {
+      // If the count reaches the specified value, trigger nextSubItem
+      nextSubItem();
+    }
+  }, [count, currentIndex, item.subItems]);
 
   const nextSubItem = () => {
     if (currentIndex < item.subItems.length - 1) {
       setCurrentIndex((prevIndex) => prevIndex + 1);
       Vibration.vibrate(50); // Vibrate for 50 milliseconds when the button is pressed
+      setCount(0);
     }
   };
 
@@ -36,25 +59,53 @@ const GenericPage = ({ route }) => {
       setCurrentIndex((prevIndex) => prevIndex - 1);
     }
   };
+  const incrementCount = () => {
+    // Check if you are already at the last index
 
+
+    // Increment the count
+    setCount((prevCount) => prevCount + 1);
+  };
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={nextSubItem}
-        disabled={currentIndex === item.subItems.length - 1}
-      >
-        {/* Display the current index and total length of sub-items */}
-        <Text>
-          {currentIndex + 1}/{item.subItems.length}
-        </Text>
-      </TouchableOpacity>
-
       <View style={[styles.rectangle, { height: maxDescriptionHeight + 100 }]}>
-        <Text style={styles.title}>{item.subItems[currentIndex].subItemName}</Text>
+        <Text style={styles.title}>
+          {item.subItems[currentIndex].subItemName}
+        </Text>
         <Text style={styles.description}>
           {item.subItems[currentIndex].subItemDescription}
         </Text>
+      </View>
+      <View style={styles.controlPan}>
+        <ImageBackground
+          source={ControlPaneBackgroundImage}
+          style={styles.ControlPaneBackground}
+        >
+          <TouchableOpacity
+            style={styles.button}
+            onPress={nextSubItem}
+            disabled={currentIndex === item.subItems.length - 1}
+          >
+            {/* Display the current index and total length of sub-items */}
+            <Text style={styles.textcount}>
+              الذكر {currentIndex + 1} من {item.subItems.length}
+            </Text>
+          </TouchableOpacity>
+          {/* Display the circular count */}
+          <TouchableOpacity 
+          style={styles.circularButton} 
+          onPress={incrementCount}
+          disabled={currentIndex === item.subItems.length - 1}>
+              <Text>
+                {count}
+              </Text>
+            </TouchableOpacity>
+          <View style={styles.button}>
+            <Text style={styles.textcount}> 
+            {item.subItems[currentIndex].repTime}
+            </Text>
+          </View>
+        </ImageBackground>
       </View>
     </View>
   );
@@ -63,20 +114,45 @@ const GenericPage = ({ route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: "#E2F0F4",
     padding: 20,
   },
+  controlPan: {
+    flex: 1,
+    justifyContent:'flex-end',
+    marginBottom:40,
+    margin:-20,
+  },
+  circularButton: {
+    width: 70,
+    height: 70,
+    borderRadius: 25,
+    borderWidth: 5,
+    borderColor: 'black',
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
   button: {
-    padding: 10,
-    marginBottom: 20,
-    backgroundColor: 'lightgray',
-    borderRadius: 5,
+    height:'50%',
+    width:'35%',
+    padding: 20,
+    backgroundColor: "#023B4F",
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: 'black',
+  },
+  textcount:{
+    textAlign:'center',
+    color:'white',
+    fontSize:20,
   },
   rectangle: {
-    backgroundColor: '#023B4F', // Background color of the rectangle
+    backgroundColor: "#023B4F", // Background color of the rectangle
     borderRadius: 10, // Border radius for the rectangle
-    alignItems: 'center',
-    justifyContent: 'space-between', // Distribute content evenly
+    alignItems: "center",
+    justifyContent: "space-between", // Distribute content evenly
     paddingTop: 60,
     paddingBottom: 30,
     marginTop: 20,
@@ -84,13 +160,20 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 20, // Adjust the font size as needed
-    textAlign: 'center',
-    color: 'white',
+    textAlign: "center",
+    color: "white",
   },
   description: {
     fontSize: 11, // Adjust the font size as needed
-    textAlign: 'center',
-    color: 'white',
+    textAlign: "center",
+    color: "white",
+  },
+  ControlPaneBackground: {
+    flexDirection:'row',
+    height: 130,
+    resizeMode: "cover", // or 'stretch' or 'contain'
+    justifyContent:'space-evenly',
+    alignItems:'center'
   },
 });
 
--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -48,7 +48,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'white', // Background of the entire screen
+    backgroundColor: "#E2F0F4",
   },
   iconWraper:{
     width:'10%',
