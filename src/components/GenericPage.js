135a83d58ec7bada776828b192adbf4132a76a85
yousefturin <yusefturin@gmail.com>
2023-09-21 11:01:54 +0300

    counter not working #2 fixed

--- src/components/GenericPage.js
+++ src/components/GenericPage.js
@@ -8,8 +8,7 @@ import {
     TouchableWithoutFeedback,
     TouchableOpacity,
 } from "react-native";
-import * as Sharing from 'expo-sharing';
-import { captureRef } from 'react-native-view-shot';
+import { handleShare } from "../utils/shareUtils";
 
 const GenericPage = ({ route }) => {
     const { item } = route.params;
@@ -21,34 +20,18 @@ const GenericPage = ({ route }) => {
     const [maxPadding, setMaxPadding] = useState(0);
     const ControlPaneBackgroundImage = require("../../assets/HeaderBackground.jpg");
 
-    const handleShare = async () => {
-        try {
-          // Capture the content of the component as an image
-          const uri = await captureRef(viewRef, {
-            format: 'png', // or 'jpeg'
-            quality: 1.0,
-          });
-    
-          // Share the captured image
-          await Sharing.shareAsync(uri, {
-            mimeType: 'image/png', // or 'image/jpeg'
-            dialogTitle: 'Share this image',
-            UTI: 'public.png', // On iOS, specify the Universal Type Identifier (UTI)
-          });
-        } catch (error) {
-          console.error('Error sharing:', error);
-        }
-      };
-    
-      let viewRef = React.createRef();
+    const viewRef = React.useRef();
 
+    const Share = async () => {
+        await handleShare(viewRef.current);
+    };
 
     useEffect(() => {
         // Find the maximum height based on the character length of subItemDescription
         let maxHeight = 150; // Default height for descriptions with less than or equal to 1000 characters
         let MaxFontSize = 20;
         let maxPadding = 60;
-        let maxpaddingHorizontal= 20;
+        let maxpaddingHorizontal = 20;
         const subItemDescription = item.subItems[currentIndex].subItemName;
 
         // Remove non-printable characters and control characters
@@ -62,54 +45,44 @@ const GenericPage = ({ route }) => {
         if (sanitizedDescription.length > 1200) {
             maxHeight = 450;
             MaxFontSize = 16;
-            maxPadding = 30; 
+            maxPadding = 30;
             maxpaddingHorizontal = 10;
-        }
-        else if (sanitizedDescription.length > 700) {
+        } else if (sanitizedDescription.length > 700) {
             maxHeight = 450;
             MaxFontSize = 16;
-            maxPadding =40; 
+            maxPadding = 40;
             maxpaddingHorizontal = 10;
-        }
-      
-        else if (sanitizedDescription.length > 600) {
+        } else if (sanitizedDescription.length > 600) {
             maxHeight = 250;
             MaxFontSize = 17;
-            maxPadding =50; 
-        }
-        else if (540 >sanitizedDescription.length > 504) {
+            maxPadding = 50;
+        } else if (540 > sanitizedDescription.length > 504) {
             maxHeight = 220;
             MaxFontSize = 17;
-            maxPadding =40; 
-        }
-        else if (sanitizedDescription.length > 500) {
+            maxPadding = 40;
+        } else if (sanitizedDescription.length > 500) {
             maxHeight = 350;
             MaxFontSize = 17;
-            maxPadding =20; 
-        }
-        else if (sanitizedDescription.length > 500) {
+            maxPadding = 20;
+        } else if (sanitizedDescription.length > 500) {
             maxHeight = 250;
             MaxFontSize = 17;
-        }
-        else if (sanitizedDescription.length > 400) {
+        } else if (sanitizedDescription.length > 400) {
             maxHeight = 200;
             MaxFontSize = 17;
-            maxPadding =35; 
-        }
-        else if (sanitizedDescription.length > 350) {
+            maxPadding = 35;
+        } else if (sanitizedDescription.length > 350) {
             maxHeight = 180;
             MaxFontSize = 17;
-            maxPadding =40; 
-        }
-        else if (sanitizedDescription.length > 210) {
+            maxPadding = 40;
+        } else if (sanitizedDescription.length > 210) {
             maxHeight = 140;
             MaxFontSize = 17;
-            maxPadding =40; 
-        }
-        else if (sanitizedDescription.length > 200) {
+            maxPadding = 40;
+        } else if (sanitizedDescription.length > 200) {
             maxHeight = 160;
             MaxFontSize = 17;
-            maxPadding =40; 
+            maxPadding = 40;
         }
 
         setMaxDescriptionHeight(maxHeight);
@@ -118,24 +91,24 @@ const GenericPage = ({ route }) => {
         setMaxpaddingHorizontal(maxpaddingHorizontal);
     }, [item.subItems, currentIndex]);
 
-
     useEffect(() => {
-        // Check if the count has reached the specified value
         if (count >= item.subItems[currentIndex].count) {
-            // If the count reaches the specified value, trigger nextSubItem
-            nextSubItem();
+            if (currentIndex < item.subItems.length - 1) {
+                nextSubItem();
+            } else {
+                // If it's the last subItem, disable counting
+                setCount(item.subItems[currentIndex].count);
+            }
         }
     }, [count, currentIndex, item.subItems]);
 
     const nextSubItem = () => {
         if (currentIndex < item.subItems.length - 1) {
             setCurrentIndex((prevIndex) => prevIndex + 1);
-            Vibration.vibrate(150); 
+            Vibration.vibrate(150);
             setCount(0);
         }
     };
-
-
 
     const incrementCount = () => {
         setCount((prevCount) => prevCount + 1);
@@ -147,28 +120,39 @@ const GenericPage = ({ route }) => {
         incrementCount();
     };
 
-
     return (
         <TouchableWithoutFeedback
             onPress={handleContainerPress}
-            disabled={currentIndex === item.subItems.length - 1}>{/*this line needs a fix for the last thikir to run the count */}
-            <View  style={styles.container}>
-            <View style={styles.containerforshare}>
-                <View  ref={viewRef} style={[styles.rectangle, { height: maxDescriptionHeight + 100 ,padding:maxPadding,paddingHorizontal:maxpaddingHorizontal}]}>
-                    <Text style={[styles.title, { fontSize: MaxFontSizeDescription }]}>
-                        {item.subItems[currentIndex].subItemName}
-                    </Text>
-                    <Text style={styles.description}>
-                        {item.subItems[currentIndex].subItemDescription}
-                    </Text>
-                    <TouchableOpacity onPress={handleShare} style={styles.shareButton}>
-        <View style={styles.dotContainer}>
-          <Text style={styles.dot}>&#8226;</Text>
-          <Text style={styles.dot}>&#8226;</Text>
-          <Text style={styles.dot}>&#8226;</Text>
-        </View>
-      </TouchableOpacity>
-                </View>
+            disabled={currentIndex === item.subItems.length - 1 && count >= item.subItems[currentIndex].count}
+        >
+            {/*this line needs a fix for the last thikir to run the count */}
+            <View style={styles.container}>
+                <View style={styles.containerforshare}>
+                    <View
+                        ref={viewRef}
+                        style={[
+                            styles.rectangle,
+                            {
+                                height: maxDescriptionHeight + 100,
+                                padding: maxPadding,
+                                paddingHorizontal: maxpaddingHorizontal,
+                            },
+                        ]}
+                    >
+                        <Text style={[styles.title, { fontSize: MaxFontSizeDescription }]}>
+                            {item.subItems[currentIndex].subItemName}
+                        </Text>
+                        <Text style={styles.description}>
+                            {item.subItems[currentIndex].subItemDescription}
+                        </Text>
+                        <TouchableOpacity onPress={Share} style={styles.shareButton}>
+                            <View style={styles.dotContainer}>
+                                <Text style={styles.dot}>&#8226;</Text>
+                                <Text style={styles.dot}>&#8226;</Text>
+                                <Text style={styles.dot}>&#8226;</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
                 </View>
                 <View style={styles.controlPan}>
                     <ImageBackground
@@ -177,7 +161,7 @@ const GenericPage = ({ route }) => {
                     >
                         <TouchableWithoutFeedback
                             onPress={nextSubItem}
-                            disabled={currentIndex === item.subItems.length - 1}
+                            disabled={currentIndex === item.subItems.length - 1 && count >= item.subItems[currentIndex].count}
                         >
                             <View style={styles.button}>
                                 {/* Display the current index and total length of sub-items */}
@@ -189,11 +173,10 @@ const GenericPage = ({ route }) => {
                         {/* Display the circular count */}
                         <TouchableWithoutFeedback
                             onPress={incrementCount}
-                            disabled={currentIndex === item.subItems.length - 1}>
+                            disabled={currentIndex === item.subItems.length - 1 && count >= item.subItems[currentIndex].count}
+                        >
                             <View style={styles.circularButton}>
-                                <Text style={styles.textcount}>
-                                    {count}
-                                </Text>
+                                <Text style={styles.textcount}>{count}</Text>
                             </View>
                         </TouchableWithoutFeedback>
                         <View style={styles.button}>
@@ -214,41 +197,40 @@ const styles = StyleSheet.create({
         backgroundColor: "#151515",
         padding: 20,
     },
-    containerforshare:{
+    containerforshare: {
         backgroundColor: "#151515",
-        height:500
+        height: 500,
     },
     controlPan: {
         flex: 1,
-        justifyContent: 'flex-end',
+        justifyContent: "flex-end",
         marginBottom: 40,
         margin: -20,
-        
     },
     circularButton: {
         width: 70,
         height: 70,
         borderRadius: 25,
         borderWidth: 3,
-        borderColor: '#151515',
+        borderColor: "#151515",
         backgroundColor: "#262626",
-        alignItems: 'center',
-        justifyContent: 'center',
+        alignItems: "center",
+        justifyContent: "center",
     },
     button: {
-        height: '50%',
-        width: '35%',
+        height: "50%",
+        width: "35%",
         padding: 14,
         backgroundColor: "#262626",
         borderRadius: 10,
         borderWidth: 2,
-        borderColor: '#151515',
+        borderColor: "#151515",
     },
     textcount: {
-        textAlign: 'center',
-        color: 'white',
+        textAlign: "center",
+        color: "white",
         fontSize: 20,
-        fontFamily: 'ScheherazadeNew'
+        fontFamily: "ScheherazadeNew",
     },
     rectangle: {
         backgroundColor: "#262626", // Background color of the rectangle
@@ -258,7 +240,7 @@ const styles = StyleSheet.create({
         paddingBottom: 30,
         padding: 50,
         marginTop: 20,
-        shadowColor: 'black',
+        shadowColor: "black",
         shadowOffset: {
             width: 0,
             height: 4,
@@ -270,21 +252,21 @@ const styles = StyleSheet.create({
     title: {
         textAlign: "center",
         color: "white",
-        fontFamily: 'ScheherazadeNew'
+        fontFamily: "ScheherazadeNew",
     },
     description: {
         fontSize: 11, // Adjust the font size as needed
         textAlign: "center",
         color: "#767676",
-        fontFamily: 'AmiriFont'
+        fontFamily: "AmiriFont",
     },
     ControlPaneBackground: {
-        flexDirection: 'row',
+        flexDirection: "row",
         height: 130,
         resizeMode: "cover", // or 'stretch' or 'contain'
-        justifyContent: 'space-evenly',
-        alignItems: 'center',
-        shadowColor: 'black',
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        shadowColor: "black",
         shadowOffset: {
             width: 0,
             height: 4,
@@ -294,19 +276,19 @@ const styles = StyleSheet.create({
         elevation: 4,
     },
     shareButton: {
-        position: 'absolute',
+        position: "absolute",
         top: 7, // Adjust the top value to position the button as desired
         left: 10, // Adjust the left value to position the button as desired
-      },
-      dotContainer: {
-        flexDirection: 'row',
-      },
-      dot: {
-        color: 'orange',
+    },
+    dotContainer: {
+        flexDirection: "row",
+    },
+    dot: {
+        color: "#be915a",
         fontSize: 20,
-        fontWeight:'700',
+        fontWeight: "700",
         marginHorizontal: 1, // Adjust the margin to control spacing between dots
-      },
+    },
 });
 
 export default GenericPage;
