a57921ded9bd73b175bdf2473d5135323b54f0ee
yousefturin <yusefturin@gmail.com>
2023-09-22 16:44:16 +0300

    added search with no logic yet, fix the display

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useState} from "react";
 import {
   View,
   Text,
   TouchableOpacity,
-  FlatList,
   StyleSheet,
   Image,
   ScrollView,
 } from "react-native";
+import { SearchBar } from 'react-native-elements';
 import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
 import { Ionicons } from "@expo/vector-icons";
 import { getItems } from "../db/GetData";
@@ -17,6 +17,14 @@ const windowWidth = Dimensions.get('window').width;
 
 const HomeScreen = ({ navigation }) => {
   const items = getItems();
+  const desiredNames = [
+    "أذكار المساء",
+    "أذكار الصباح",
+    " أذكار بعد الصلاة",
+    "أذكار النوم",
+  ];
+  // Use the filter method to extract items with the desired names
+  const filteredItems = items.filter(item => desiredNames.includes(item.name));
 
   const renderBorderRadius = (index) => {
     if (index === 0) {
@@ -32,86 +40,132 @@ const HomeScreen = ({ navigation }) => {
     }
     return {};
   };
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchMode, setSearchMode] = useState(false);
 
-  // Extract the first 4 items for the new layout
-  const topItems = items.slice(0, 4);
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
+
+  const handleSearchBarClick = () => {
+    setSearchMode(true);
+  };
+
+  const handleCancel = () => {
+    setSearchQuery('');
+    setSearchMode(false);
+  };
+
+
+
+
   const topItemIcons = ["moon", "partly-sunny", "notifications", "bed"];
 
   return (
     <View style={styles.pageContainer}>
-      <ScrollView contentContainerStyle={styles.container}>
-        <View style={styles.buttonGrid}>
-          {topItems.map((item, index) => (
-            <TouchableOpacity
-              key={item.name}
-              style={[styles.squareButton, renderBorderRadius(index)]}
-              onPress={() =>
-                navigation.navigate("GenericPage", {
-                  name: item.name,
-                  item: item,
-                })
-              }
-              activeOpacity={0.7}
-            >
-              <View style={styles.iconWrapperTop}>
-                <Ionicons
-                  name={topItemIcons[index]}
-                  size={35}
-                  color="#fff"
-                  style={styles.iconTop}
-                />
-              </View>
-              <View style={styles.nameWrapper}>
-                <Text style={styles.buttonTextTop}>{item.name}</Text>
-              </View>
-            </TouchableOpacity>
-          ))}
-        </View>
-        <View style={styles.TextMidWrapper}>
-          <Text style={styles.TextMid}>الفهرس</Text>
-        </View>
-        {items.map((item, index) => (
-          <View key={item.name}>
-            <TouchableOpacity
-              style={[styles.button, renderBorderRadius(index)]}
-              onPress={() =>
-                navigation.navigate("GenericPage", {
-                  name: item.name,
-                  item: item,
-                })
-              }
-              activeOpacity={0.7}
-            >
-              <View style={styles.iconWrapper}>
-                <FontAwesomeIcon
-                  name="angle-left"
-                  size={24}
-                  color="#454545"
-                  style={styles.icon}
-                />
-              </View>
-              <View style={styles.nameWrapper}>
-                <Text style={styles.buttonText}>{item.name}</Text>
-              </View>
-              <View style={styles.imageWrapper}>
-                {/* Image component */}
-                <Image style={styles.image} />
-              </View>
-            </TouchableOpacity>
-            {index !== items.length - 1 && (
-              <View style={styles.horizontalLine} />
-            )}
-          </View>
+    <ScrollView
+      contentContainerStyle={styles.container}
+      contentOffset={{ x: 0, y: 100 }}
+      scrollEnabled={!searchMode} // Prevent scrolling when searchMode is true
+    >
+      <SearchBar
+        placeholder="بحث..."
+        onChangeText={handleSearch}
+        value={searchQuery}
+        platform="ios"
+        containerStyle={styles.searchBarContainer}
+        inputContainerStyle={styles.searchBarInputContainer}
+        inputStyle={styles.searchBarInput}
+        onFocus={handleSearchBarClick}
+        onCancel={handleCancel}
+        showCancel
+        cancelButtonTitle="الغاء"
+      />
+      <View style={styles.buttonGrid}>
+        {filteredItems.map((item, index) => (
+          <TouchableOpacity
+            key={item.name}
+            style={[
+              styles.squareButton,
+              renderBorderRadius(index),
+              searchMode && styles.disabledButton,
+            ]}
+            onPress={() =>
+              navigation.navigate("GenericPage", {
+                name: item.name,
+                item: item,
+              })
+            }
+            activeOpacity={searchMode ? 1 : 0.7}
+            disabled={searchMode}
+          >
+            <View style={styles.iconWrapperTop}>
+              <Ionicons
+                name={topItemIcons[index]}
+                size={35}
+                color="#fff"
+                style={styles.iconTop}
+              />
+            </View>
+            <View style={styles.nameWrapper}>
+              <Text style={styles.buttonTextTop}>{item.name}</Text>
+            </View>
+          </TouchableOpacity>
         ))}
-      </ScrollView>
-    </View>
-  );
-};
+      </View>
+      <View style={styles.TextMidWrapper}>
+        <Text style={styles.TextMid}>الفهرس</Text>
+      </View>
+      {items.map((item, index) => (
+        <View key={item.name}>
+          <TouchableOpacity
+            style={[
+              styles.button,
+              renderBorderRadius(index),
+              searchMode && styles.disabledButton,
+            ]}
+            onPress={() =>
+              navigation.navigate("GenericPage", {
+                name: item.name,
+                item: item,
+              })
+            }
+            activeOpacity={searchMode ? 1 : 0.7}
+            disabled={searchMode}
+          >
+            <View style={styles.iconWrapper}>
+              <FontAwesomeIcon
+                name="angle-left"
+                size={24}
+                color="#454545"
+                style={styles.icon}
+              />
+            </View>
+            <View style={styles.nameWrapper}>
+              <Text style={styles.buttonText}>{item.name}</Text>
+            </View>
+            <View style={styles.imageWrapper}>
+              {/* Image component */}
+              <Image style={styles.image} />
+            </View>
+          </TouchableOpacity>
+          {index !== items.length - 1 && (
+            <View style={styles.horizontalLine} />
+          )}
+        </View>
+      ))}
+    </ScrollView>
+  </View>
+);
+}
 
 const styles = StyleSheet.create({
   pageContainer: {
     flex: 1,
     backgroundColor: "#151515", // Background color for the entire page
+  },
+  disabledButton: {
+    opacity: 0.5, // Adjust the opacity as needed when the button is disabled
   },
   container: {
     flexGrow: 1,
@@ -119,7 +173,20 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     paddingBottom: 80,
-    paddingTop: 30,
+    paddingTop: 20,
+  },
+  searchBarContainer: {
+    paddingHorizontal: 10,
+    backgroundColor: "#151515",
+    borderBottomColor: 'transparent',
+    borderTopColor: 'transparent',
+  },
+  searchBarInputContainer: {
+    backgroundColor: '#262626',
+  },
+  searchBarInput: {
+    backgroundColor: '#262626',
+    color: '#dddddd'
   },
   iconWrapper: {
     width: "10%",
@@ -167,7 +234,7 @@ const styles = StyleSheet.create({
     marginLeft: windowWidth > 600 ? 610 : 350, // Adjust the value based on screen width
   },
   buttonGrid: {
-    paddingTop: 30,
+    paddingTop: 10,
     paddingBottom: 20,
     width: "91%",
     flexDirection: "row",
@@ -212,9 +279,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
     alignItems: 'flex-end', // Change alignItems to 'flex-start'
     marginRight: windowWidth > 600 ? 60 : 35, // Adjust the value based on screen width
-
   },
-  
   TextMid: {
     color: 'white',
     fontSize: 22,
