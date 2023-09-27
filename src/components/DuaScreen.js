883476843e4567ae8b540283252d8d14eab9d4fa
yousefturin <yusefturin@gmail.com>
2023-09-27 18:10:27 +0300

    removed unneeded import

--- src/components/DuaScreen.js
+++ src/components/DuaScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { fetchRandomDuaFromFile } from "../API/GETDuaArbEn";
 import { handleShare } from "../utils/shareUtils";
