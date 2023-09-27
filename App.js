f6fb010abb6e77a799e01edb14a5e0b8cfae6d79
yousefturin <yusefturin@gmail.com>
2023-09-27 13:25:37 +0300

    fixed the them problem for navbar

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./src/navigation/AppNavigator";
 import * as Font from "expo-font";
+import { ThemeProvider, useTheme } from "./src/context/ThemContex";
+
 const App = () => {
   const [fontLoaded, setFontLoaded] = useState(false);
 
@@ -17,30 +19,31 @@ const App = () => {
     }
     const requestNotificationPermissions = async () => {
       const { status } = await Notifications.requestPermissionsAsync();
-      if (status === 'granted') {
-        console.log('Notification permissions granted');
-        
+      if (status === "granted") {
+        console.log("Notification permissions granted");
+
         // Configure notification categories
-        Notifications.setNotificationCategoryAsync('alarm', [
+        Notifications.setNotificationCategoryAsync("alarm", [
           {
-            identifier: 'dismiss',
-            buttonTitle: 'Dismiss',
+            identifier: "dismiss",
+            buttonTitle: "Dismiss",
           },
         ]);
       } else {
-        console.log('Notification permissions denied');
+        console.log("Notification permissions denied");
       }
     };
     loadFonts();
   }, []);
 
-  
   if (!fontLoaded) {
     return console.log(!fontLoaded); // Display a loading indicator
   }
   return (
     <NavigationContainer>
-      <AppNavigator />
+      <ThemeProvider>
+        <AppNavigator />
+      </ThemeProvider>
     </NavigationContainer>
   );
 };
