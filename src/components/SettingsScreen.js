0d9cbdc3b7fdb0aafe175128e312dfb1d7a3602f
yousefturin <yusefturin@gmail.com>
2023-09-27 13:28:24 +0300

    added toogle for theme mode

--- src/components/SettingsScreen.js
+++ src/components/SettingsScreen.js
@@ -1,16 +1,59 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Switch } from 'react-native';
+import { useTheme } from '../context/ThemContex'; // Import the useTheme hook from your ThemeContext file
 
 const SettingScreen = ({ navigation }) => {
+  const { isDarkMode, toggleTheme } = useTheme(); // Access theme state and toggle function
+
+  const lightTheme = {
+    backgroundColor: "#f2f2f6", 
+    textColor: '#000000',
+    // Add other light theme styles here
+  };
+
+  const darkTheme = {
+    backgroundColor: '#151515',
+    textColor: '#FFFFFF',
+    // Add other dark theme styles here
+  };
+
+  const currentTheme = isDarkMode ? darkTheme : lightTheme;
+
   return (
-    <View>
+    <View style={[styles.container, { backgroundColor: currentTheme.backgroundColor }]}>
       {/* Navigation bar */}
       {/* Add your navigation bar here */}
-      <Text>Setting Page</Text>
       <TouchableOpacity onPress={() => navigation.navigate('Menu')}>
+        {/* Content */}
       </TouchableOpacity>
+      
+      {/* iOS-style toggle */}
+      <View style={styles.toggleContainer}>
+        <Text style={{ color: currentTheme.textColor }}>Dark Mode</Text>
+        <Switch
+          value={isDarkMode}
+          onValueChange={toggleTheme}
+          thumbColor={isDarkMode ? '#fefffe' : '#fefffe'} 
+                  trackColor={{ true: '#be915a', false: '#454545' }}
+        />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    // Add other common styles here
+  },
+  toggleContainer: {
+    flexDirection:"row-reverse",
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginVertical: 20,
+    paddingHorizontal: 16,
+  },
+  // Add other styles as needed
+});
 
 export default SettingScreen;
