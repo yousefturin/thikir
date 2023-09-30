bfdfcaa3df4a1044ad7849e6331a3495fb5abd1f
yousefturin <yusefturin@gmail.com>
2023-09-30 13:57:59 +0300

    fixed the styling for displayign the font picker

--- src/context/ThemContex.js
+++ src/context/ThemContex.js
@@ -1,24 +1,41 @@
-// 1. ThemeContext.js
-import React, { createContext, useContext, useState } from 'react';
+// ThemeContext.js
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// 2. Define Themes
+// Define Themes
 const lightTheme = {
   backgroundColor: '#FFFFFF',
   textColor: '#000000',
-
 };
 
 const darkTheme = {
   backgroundColor: '#151515',
-  textColor: '#fff',
-
+  textColor: '#FFFFFF',
 };
 
 // Create a ThemeContext
 const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(null); // Use null to represent initial loading state
+
+  useEffect(() => {
+    async function fetchTheme() {
+      try {
+        const themeValue = await AsyncStorage.getItem('@selectedTheme');
+        if (themeValue === 'dark') {
+          setIsDarkMode(true);
+        } else {
+          setIsDarkMode(false);
+        }
+      } catch (error) {
+        console.error('Error fetching theme:', error);
+        setIsDarkMode(false); // Set the default theme in case of an error
+      }
+    }
+
+    fetchTheme();
+  }, []);
 
   // Toggle Theme
   const toggleTheme = () => {
@@ -26,7 +43,19 @@ export function ThemeProvider({ children }) {
   };
 
   // Get the current theme
-  const currentTheme = isDarkMode ? darkTheme : lightTheme;
+  const currentTheme = isDarkMode === null ? lightTheme : isDarkMode ? darkTheme : lightTheme;
+
+  useEffect(() => {
+    // Save the selected theme to AsyncStorage whenever it changes
+    if (isDarkMode !== null) {
+      AsyncStorage.setItem('@selectedTheme', isDarkMode ? 'dark' : 'light');
+    }
+  }, [isDarkMode]);
+
+  if (isDarkMode === null) {
+    // Return null while the theme is loading
+    return null;
+  }
 
   return (
     <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
