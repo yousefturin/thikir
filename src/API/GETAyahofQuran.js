5668e9dfe426adee8d61724439376eb729d33056
yousefturin <yusefturin@gmail.com>
2023-09-30 13:54:23 +0300

    added a new apiGet for the tafsir of verse

--- src/API/GETAyahofQuran.js
+++ src/API/GETAyahofQuran.js
@@ -1,39 +1,46 @@
 const fetchRandomVerse = async () => {
-    try {
-      const randomAyah = Math.floor(Math.random() * 6236) + 1; // Generate a random ayah number between 1 and 6236
-      const edition = 'Ben.asad';
-  
-      const apiUrl = `http://api.alquran.cloud/v1/ayah/${randomAyah}/${edition}`;
-      const response = await fetch(apiUrl);
-  
-      if (!response.ok) {
-        throw new Error('Failed to fetch data');
-      }
-  
-      const data = await response.json();
-  
-      if (data?.data?.text) {
-        // Extract the surah name and ayah number from the response
-        const surahName = data.data.surah.name;
-        const ayahNumber = data.data.numberInSurah;
-        const verseText = data.data.text;
-        return {
-          surahName,
-          ayahNumber,
-          verseText,
-        };
-      } else {
-        throw new Error('Invalid response data structure');
-      }
-    } catch (error) {
-      console.error('Error fetching Quranic verse:', error);
-      return {
-        surahName: 'Error',
-        ayahNumber: 'Error',
-        verseText: 'Error fetching Quranic verse.',
-      };
+  try {
+    const randomAyah = Math.floor(Math.random() * 6236) + 1; // Generate a random ayah number between 1 and 6236
+
+    const editionForVerse = 'Ben.asad';
+
+    const apiUrlVerse = `http://api.alquran.cloud/v1/ayah/${randomAyah}/${editionForVerse}`;
+    const response = await fetch(apiUrlVerse);
+    const editionForTafser = 'ar.muyassar';
+    const apiUrlTafser = `http://api.alquran.cloud/v1/ayah/${randomAyah}/${editionForTafser}`;
+    const responseTafser = await fetch(apiUrlTafser);
+
+    if (!response.ok && !responseTafser.ok) {
+      throw new Error('Failed to fetch data for Verse');
     }
-  };
-  
-  export { fetchRandomVerse };
-  
+
+    const tafsirResponse = await responseTafser.json(); 
+    const data = await response.json();
+
+    if (data?.data?.text && tafsirResponse?.data?.text) {
+      // Extract the surah name and ayah number from the response
+      const surahName = data.data.surah.name;
+      const ayahNumber = data.data.numberInSurah;
+      const verseText = data.data.text;
+      const tafsirText = tafsirResponse.data.text; 
+      return {
+        surahName,
+        ayahNumber,
+        verseText,
+        tafsirText, 
+      };
+    } else {
+      throw new Error('Invalid response data structure');
+    }
+  } catch (error) {
+    console.error('Error fetching Quranic verse:', error);
+    return {
+      surahName: 'Error',
+      ayahNumber: 'Error',
+      verseText: 'Error fetching Quranic verse.',
+      tafsirText: 'Error fetching a Tafsir verse',
+    };
+  }
+};
+
+export { fetchRandomVerse };
