e32e6f54ec991c66b113b704a39c28a0f0a118b6
yousefturin <yusefturin@gmail.com>
2023-10-03 21:59:27 +0300

    changes in pakages

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./src/navigation/AppNavigator";
 import * as Font from "expo-font";
+import { Platform } from 'react-native';
+import * as Notifications from "expo-notifications";
 import { ThemeProvider  } from "./src/context/ThemContex";
 import { FontProvider } from "./src/context/FontContext";
 const App = () => {
@@ -21,21 +23,39 @@ const App = () => {
   
   useEffect(() => {
     const requestNotificationPermissions = async () => {
-      const { status } = await Notifications.requestPermissionsAsync();
-      if (status === "granted") {
-        console.log("Notification permissions granted");
-
-        // Configure notification categories
-        Notifications.setNotificationCategoryAsync("alarm", [
-          {
-            identifier: "dismiss",
-            buttonTitle: "Dismiss",
-          },
-        ]);
-      } else {
-        console.log("Notification permissions denied");
+      if (Platform.OS === 'android'){
+        const { status } = await Notifications.requestPermissionsAsync();
+        if (status === "granted") {
+          console.log("Notification permissions granted");
+  
+          // Configure notification categories
+          Notifications.setNotificationCategoryAsync("alarm", [
+            {
+              identifier: "dismiss",
+              buttonTitle: "Dismiss",
+            },
+          ]);
+        } else {
+          console.log("Notification permissions denied");
+        }
+      }else{
+        const { status } = await Notifications.requestPermissionsAsync();
+        if (status === "granted") {
+          console.log("Notification permissions granted");
+  
+          // Configure notification categories
+          Notifications.setNotificationCategoryAsync("alarm", [
+            {
+              identifier: "dismiss",
+              buttonTitle: "Dismiss",
+            },
+          ]);
+        } else {
+          console.log("Notification permissions denied");
+        }
       }
     };
+    requestNotificationPermissions();
   }, []);
 
   if (!fontLoaded) {
