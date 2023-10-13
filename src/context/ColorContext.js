34478de6e509df28b05c9fb150f3e09436518cce
yousefturin <yusefturin@gmail.com>
2023-10-13 21:31:12 +0300

    fixed the colorContext

--- src/context/ColorContext.js
+++ src/context/ColorContext.js
@@ -1,45 +1,44 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-
 const ColorContext = createContext();
 
 export function ColorProvider({ children }) {
-    const [selectedColor, setSelectedColor] = useState('#f2b784'); // Default font selection
+    const [selectedColor, setSelectedColor] = useState(null); // Initialize with null or a loading state
 
+    // Fetch the color from AsyncStorage before rendering
     useEffect(() => {
-        // Load the selected font from AsyncStorage on app startup
         async function fetchColor() {
             try {
                 const colorValue = await AsyncStorage.getItem('@selectedColor');
                 if (colorValue) {
                     setSelectedColor(colorValue);
+                } else {
+                    setSelectedColor('#f2b784'); // Default color if not found
                 }
             } catch (error) {
-                console.error('Error fetching font:', error);
+                console.error('Error fetching color:', error);
+                setSelectedColor('#f2b784'); // Default color in case of an error
             }
         }
 
         fetchColor();
     }, []);
 
-    // Set the selected font
-    const setColor = (color) => {
+    // Set the selected color
+    const setColor = async (color) => {
         setSelectedColor(color);
-        // Save the selected font to AsyncStorage
-        AsyncStorage.setItem('@selectedColor', color);
+        try {
+            // Save the selected color to AsyncStorage
+            await AsyncStorage.setItem('@selectedColor', color);
+        } catch (error) {
+            console.error('Error saving color:', error);
+        }
     };
 
-    useEffect(() => {
-        // Save the selected theme to AsyncStorage whenever it changes
-        if (selectedColor !== null) {
-            AsyncStorage.setItem('@selectedColor', selectedColor);
-        }
-    }, [selectedColor]);
-
     if (selectedColor === null) {
-        // Return null while the theme is loading
-        return null;
+        // Return a loading state or a placeholder while the color is loading
+        return 
     }
 
     return (
@@ -48,6 +47,10 @@ export function ColorProvider({ children }) {
         </ColorContext.Provider>
     );
 }
+
+
+
+
 
 export function useColor() {
     return useContext(ColorContext);
