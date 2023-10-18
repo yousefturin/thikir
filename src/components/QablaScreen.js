e9101089bdc0b7920b8425f12afac7dcbf18f943
yousefturin <yusefturin@gmail.com>
2023-10-18 13:50:25 +0300

    new screen is added for qabla

---
+++ src/components/QablaScreen.js
@@ -0,0 +1,137 @@
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Dimensions } from "react-native";
+import * as Location from 'expo-location';
+import { DeviceMotion } from 'expo-sensors';
+
+const QiblaScreen = () => {
+  const [location, setLocation] = useState(null);
+  const [qiblaDirection, setQiblaDirection] = useState(null);
+  const [compassRotation, setCompassRotation] = useState(0);
+
+  useEffect(() => {
+    getLocation();
+    startCompass();
+  }, []);
+
+  const getLocation = async () => {
+    try {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+        return;
+      }
+
+      const { coords } = await Location.getCurrentPositionAsync({});
+      const { latitude, longitude } = coords;
+
+      setLocation({ latitude, longitude });
+      fetchQiblaDirection(latitude, longitude);
+    } catch (error) {
+      console.error('Error getting location', error);
+    }
+  };
+
+  const fetchQiblaDirection = (latitude, longitude) => {
+    const apiUrl = `http://api.aladhan.com/v1/qibla/${latitude}/${longitude}`;
+
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setQiblaDirection(data.data.direction);
+      })
+      .catch((error) => {
+        console.error('Error fetching Qibla direction:', error);
+      });
+  };
+
+  const startCompass = async () => {
+    DeviceMotion.addListener((data) => {
+      if (data.rotation) {
+        const { alpha } = data.rotation;
+        updateCompassRotation(alpha || 0);
+      }
+    });
+  };
+
+  const updateCompassRotation = (alpha) => {
+    if (qiblaDirection !== null) {
+      // Calculate the new rotation of the arrow based on Qibla Direction and Compass Rotation.
+      const newRotation = qiblaDirection - alpha; // Note the subtraction here.
+      setCompassRotation(newRotation);
+    }
+  };
+
+  const renderNumbers = () => {
+    const numbers = [];
+    for (let i = 0; i < 360; i += 15) {
+      numbers.push(
+        <Text
+          key={i}
+          style={{
+            position: 'absolute',
+            top: Dimensions.get('window').height / 2 - 280,
+            left: Dimensions.get('window').width / 2 - 85,
+            transform: [{ rotate: `${i}deg` },
+            { translateY: -120 }]
+          }}
+        >
+          {i}
+        </Text>
+      );
+    }
+    return numbers;
+  };
+
+  return (
+    <View style={styles.container}>
+      {qiblaDirection !== null && (
+        <Text>
+          Qibla Direction: {qiblaDirection} degrees
+        </Text>
+      )}
+      <View style={styles.compassContainer}>
+        <Text style={styles.compassText}>
+          Compass Rotation: {Math.round(compassRotation)} degrees
+        </Text>
+        <View style={styles.compass}>
+          {renderNumbers()}
+          <View style={[styles.arrow, { transform: [{ rotate: `${compassRotation}deg` }] }]} />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  compassContainer: {
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  compass: {
+    width: 250,
+    height: 250,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  arrowContainer: {
+    justifyContent: 'flex-end', // Adjust the positioning
+    alignItems: 'center',
+    width: 3,
+    height: 120,
+  },
+  arrow: {
+    width: 3,
+    height: 120,
+    backgroundColor: 'red',
+  },
+  compassText: {
+    fontSize: 18,
+  },
+});
+
+export default QiblaScreen;
