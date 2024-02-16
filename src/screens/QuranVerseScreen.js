f15d87d7810307a28c0c3598d171dcae6eef1ba3
yousefturin <yusefturin@gmail.com>
2024-02-16 09:42:33 +0300

    skeleton UI is applied, for better UX

--- src/screens/QuranVerseScreen.js
+++ src/screens/QuranVerseScreen.js
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, TouchableOpacity, StyleSheet, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+  Animated,
+} from "react-native";
 import { handleShare } from "../Service/ShareService";
-import { useTheme } from '../context/ThemeContext'; 
+import { useTheme } from "../context/ThemeContext";
 import { useFont } from "../context/FontContext";
-import { useColor } from '../context/ColorContext';
+import { useColor } from "../context/ColorContext";
 import { useLanguage } from "../context/LanguageContext";
-import { QuranVerseStyles } from '../Styles/commonStyles';
-import { useNumberContext } from '../context/NumberContext';
+import { QuranVerseStyles } from "../Styles/commonStyles";
+import { useNumberContext } from "../context/NumberContext";
 import { getColorForTheme } from "../utils/themeUtils";
 import { useRandomVerse } from "../Service/QuranVerseService";
 
@@ -15,62 +22,62 @@ const QuranVerseScreen = ({ navigation }) => {
   const { selectedFont } = useFont();
   const { selectedColor } = useColor();
   const { selectedLanguage } = useLanguage();
-  const { state, convertToEasternArabicNumerals } = useNumberContext(); 
-  const systemTheme = selectedTheme === 'system';
+  const { state, convertToEasternArabicNumerals } = useNumberContext();
+  const systemTheme = selectedTheme === "system";
 
   const verseData = useRandomVerse();
 
   //#region selectedFont
   const HafsFont = StyleSheet.create({
-      title:{
-          fontFamily:"Hafs",
-      }
+    title: {
+      fontFamily: "Hafs",
+    },
   });
   const ScheherazadeNewFont = StyleSheet.create({
-      title:{
-          fontFamily:"ScheherazadeNew",
-      }
+    title: {
+      fontFamily: "ScheherazadeNew",
+    },
   });
   const MeQuranFont = StyleSheet.create({
-      title:{
-          fontFamily:"MeQuran",
-      }
+    title: {
+      fontFamily: "MeQuran",
+    },
   });
   //#endregion
 
-  //#region LightTheme 
+  //#region LightTheme
   const lightTheme = StyleSheet.create({
     container: {
-      backgroundColor: "#f2f2f6", 
+      backgroundColor: "#f2f2f6",
     },
     rectangle: {
       backgroundColor: "#fefffe",
-      shadowColor: "gray", 
+      shadowColor: "gray",
     },
     title: {
       color: "#000",
     },
     horizontalLine: {
       borderColor: "#f2f2f6",
-  },
+    },
   });
   //#endregion
-  
+
   //#region DarkTheme
   const darkTheme = StyleSheet.create({
     container: {
-      backgroundColor: "#151515", 
+      backgroundColor: "#151515",
     },
     rectangle: {
       backgroundColor: "#262626",
-      shadowColor: "black", 
+      shadowColor: "black",
     },
     title: {
       color: "#fff",
     },
     horizontalLine: {
       borderColor: "#151515",
-  },
+    },
   });
   //#endregion
   const themeStyles = getColorForTheme(
@@ -84,21 +91,31 @@ const QuranVerseScreen = ({ navigation }) => {
     ...QuranVerseStyles,
     container: {
       ...QuranVerseStyles.container,
-      ...selectedTheme  === 'dark'? themeStyles.container : themeStyles.container, 
+      ...(selectedTheme === "dark"
+        ? themeStyles.container
+        : themeStyles.container),
     },
     rectangle: {
-      ...QuranVerseStyles.rectangle, 
-      ...selectedTheme  === 'dark'? themeStyles.rectangle : themeStyles.rectangle, 
+      ...QuranVerseStyles.rectangle,
+      ...(selectedTheme === "dark"
+        ? themeStyles.rectangle
+        : themeStyles.rectangle),
     },
     title: {
-      ...QuranVerseStyles.title, 
-      ...selectedTheme  === 'dark'? themeStyles.title : themeStyles.title, 
-      ...(selectedFont === 'MeQuran' ? MeQuranFont.title : (selectedFont === 'ScheherazadeNew' ? ScheherazadeNewFont.title : HafsFont.title)),
+      ...QuranVerseStyles.title,
+      ...(selectedTheme === "dark" ? themeStyles.title : themeStyles.title),
+      ...(selectedFont === "MeQuran"
+        ? MeQuranFont.title
+        : selectedFont === "ScheherazadeNew"
+        ? ScheherazadeNewFont.title
+        : HafsFont.title),
     },
     horizontalLine: {
       ...QuranVerseStyles.horizontalLine,
-      ...(selectedTheme  === 'dark'? themeStyles.horizontalLine : themeStyles.horizontalLine),
-  },
+      ...(selectedTheme === "dark"
+        ? themeStyles.horizontalLine
+        : themeStyles.horizontalLine),
+    },
   };
   //#endregion
 
@@ -107,8 +124,7 @@ const QuranVerseScreen = ({ navigation }) => {
   const [maxpaddingHorizontal, setMaxpaddingHorizontal] = useState(20);
   const [textLength, setTextLength] = useState(verseData.verseTextLength);
   const viewRef = React.useRef();
-  //#endregion
-  
+
   useEffect(() => {
     let MaxFontSize = 20;
     let maxPadding = 30;
@@ -128,61 +144,157 @@ const QuranVerseScreen = ({ navigation }) => {
     setMaxPadding(maxPadding);
     setMaxpaddingHorizontal(maxpaddingHorizontal);
   }, [textLength]);
-  
-    const textStyle = {
-      fontSize: maxFontSizeDescription,
-      padding: maxPadding,
-      paddingHorizontal: maxpaddingHorizontal,
-    };
-    //#endregion
 
+  const textStyle = {
+    fontSize: maxFontSizeDescription,
+    padding: maxPadding,
+    paddingHorizontal: maxpaddingHorizontal,
+  };
+  //#endregion
 
   const Share = async () => {
     await handleShare(viewRef.current);
   };
 
+  //#region animation
+  const [animatedValue] = useState(new Animated.Value(0));
+  const AnimationBackgroundTheme = getColorForTheme(
+    {
+      dark: ["hsl(0, 0%, 29.8%)", "hsl(0, 0%, 17.8%)"],
+      light: ["hsl(0, 0%, 94.9%)", "hsl(0, 0%, 90.2%)"],
+    },
+    selectedTheme,
+    systemTheme
+  );
+  useEffect(() => {
+    startAnimation();
+  }, []);
 
-  
+  const startAnimation = () => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(animatedValue, {
+          toValue: 1,
+          duration: 1000,
+          useNativeDriver: false,
+        }),
+        Animated.timing(animatedValue, {
+          toValue: 0,
+          duration: 1000,
+          useNativeDriver: false,
+        }),
+      ]),
+      { iterations: -1 }
+    ).start();
+  };
+
+  const backgroundColorInterpolation = animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: AnimationBackgroundTheme,
+  });
+  //#endregion
+  // 
   return (
-
     <View ref={viewRef} style={styles.container}>
-      <View style={[styles.rectangle, textStyle]}>
-      <ScrollView
-          contentContainerStyle={styles.scrollContainer}
-          showsVerticalScrollIndicator={false}
+      {Object.values(verseData).some(Boolean) ? (
+        <View style={[styles.rectangle, textStyle]}>
+          <ScrollView
+            contentContainerStyle={styles.scrollContainer}
+            showsVerticalScrollIndicator={false}
           >
-        <Text style={[styles.title, textStyle]}>
-          {verseData.verseText} 
-          ﴿ 
-          {ayahNumberToDisplay = state.isArabicNumbers
-          ? convertToEasternArabicNumerals(verseData.ayahNumber.toString())
-          : verseData.ayahNumber.toString()}﴾
-        </Text>
-        <View style={styles.horizontalLine} />
-        <Text style={[styles.tafsirStyle,
-        {fontFamily:selectedLanguage!="Arabic"? "Montserrat":"AmiriFont",
-        textAlign:selectedLanguage!="Arabic"? "left": "right",
-        }]}>
-        {selectedLanguage!="Arabic"?verseData.EnTafsir:verseData.tafsir}</Text>
+            <Text style={[styles.title, textStyle]}>
+              {verseData.verseText}﴿
+              {
+                (ayahNumberToDisplay = state.isArabicNumbers
+                  ? convertToEasternArabicNumerals(
+                      verseData.ayahNumber.toString()
+                    )
+                  : verseData.ayahNumber.toString())
+              }
+              ﴾
+            </Text>
+            <View style={styles.horizontalLine} />
+            <Text
+              style={[
+                styles.tafsirStyle,
+                {
+                  fontFamily:
+                    selectedLanguage != "Arabic" ? "Montserrat" : "AmiriFont",
+                  textAlign: selectedLanguage != "Arabic" ? "left" : "right",
+                },
+              ]}
+            >
+              {selectedLanguage != "Arabic"
+                ? verseData.EnTafsir
+                : verseData.tafsir}
+            </Text>
 
-        <Text style={styles.description}>
-        [ {selectedLanguage!= "Arabic"?verseData.surahNameEn:verseData.surahName} ]</Text>
-        </ScrollView>
-        <TouchableOpacity
-          onPress={() => navigation.navigate("Menu")}
-          style={styles.shareButton}
-        ></TouchableOpacity>
-        <TouchableOpacity onPress={Share} style={styles.shareButton}>
-          <View style={styles.dotContainer}>
-            <Text style={[styles.dot,{color:selectedColor}]}>&#8226;</Text>
-            <Text style={[styles.dot,{color:selectedColor}]}>&#8226;</Text>
-            <Text style={[styles.dot,{color:selectedColor}]}>&#8226;</Text>
+            <Text style={styles.description}>
+              [{" "}
+              {selectedLanguage != "Arabic"
+                ? verseData.surahNameEn
+                : verseData.surahName}{" "}
+              ]
+            </Text>
+          </ScrollView>
+          <TouchableOpacity onPress={Share} style={styles.shareButton}>
+            <View style={styles.dotContainer}>
+              <Text style={[styles.dot, { color: selectedColor }]}>
+                &#8226;
+              </Text>
+              <Text style={[styles.dot, { color: selectedColor }]}>
+                &#8226;
+              </Text>
+              <Text style={[styles.dot, { color: selectedColor }]}>
+                &#8226;
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <View
+          style={[
+            styles.rectangle,
+            {
+              padding: 20,
+              paddingHorizontal: 20,
+              justifyContent: "space-around",
+            },
+          ]}
+        >
+          <Animated.View
+            style={[
+              styles.skeletonTitle,
+              { backgroundColor: backgroundColorInterpolation },
+            ]}
+          />
+          <View style={[styles.horizontalLine]} />
+          <Animated.View
+            style={[
+              styles.skeletonTafsirStyle,
+              { backgroundColor: backgroundColorInterpolation },
+            ]}
+          />
+          <Animated.View
+            style={[
+              styles.skeletonDescription,
+              { backgroundColor: backgroundColorInterpolation },
+            ]}
+          />
+          <View style={styles.shareButton}>
+            <Animated.View style={styles.dotContainer}>
+              <Animated.View
+                style={[
+                  styles.skeletonDotPlace,
+                  { backgroundColor: backgroundColorInterpolation },
+                ]}
+              />
+            </Animated.View>
           </View>
-        </TouchableOpacity>
-      </View>
+        </View>
+      )}
     </View>
   );
 };
-
 
 export default QuranVerseScreen;
