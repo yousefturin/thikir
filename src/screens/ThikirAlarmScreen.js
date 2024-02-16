fae188ecc0bacbf989b9038df1cd334a75c11e1c
yousefturin <yusefturin@gmail.com>
2024-02-16 09:43:32 +0300

    code re-shaped for better reading

--- src/screens/ThikirAlarmScreen.js
+++ src/screens/ThikirAlarmScreen.js
@@ -1,24 +1,33 @@
 import React, { useEffect, useState } from "react";
 
-  import { View, Text, TouchableOpacity, Switch, StyleSheet, Platform, Dimensions,} from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Switch,
+  StyleSheet,
+  Platform,
+  Dimensions,
+} from "react-native";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
-import DateTimePicker from '@react-native-community/datetimepicker';
+// import DateTimePicker from "@react-native-community/datetimepicker";
 import * as Notifications from "expo-notifications";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useTheme } from '../context/ThemeContext';
-import { useColor } from '../context/ColorContext';
-import { ThikirAlarmStyles } from '../Styles/commonStyles';
-import { useNumberContext } from '../context/NumberContext';
+import { useTheme } from "../context/ThemeContext";
+import { useColor } from "../context/ColorContext";
+import { ThikirAlarmStyles } from "../Styles/commonStyles";
+import { useNumberContext } from "../context/NumberContext";
 import { useLanguage } from "../context/LanguageContext";
-import { Appearance } from 'react-native';
-const {width} = Dimensions.get("window");
+import { Appearance } from "react-native";
+const { width } = Dimensions.get("window");
 
 const ThikirAlarmScreen = () => {
   const { selectedTheme } = useTheme();
   const { selectedColor } = useColor();
   const { selectedLanguage } = useLanguage();
-  const { state, convertToEasternArabicNumerals } = useNumberContext(); 
-  const systemTheme = selectedTheme === 'system'; 
+  const { state, convertToEasternArabicNumerals } = useNumberContext();
+  const systemTheme = selectedTheme === "system";
+  
   //#region LightTheme
   const lightTheme = StyleSheet.create({
     container: {
@@ -26,7 +35,7 @@ const ThikirAlarmScreen = () => {
     },
     notificationContainer: {
       backgroundColor: "#fefffe",
-      shadowColor: "gray", 
+      shadowColor: "gray",
     },
     title: {
       color: "#000",
@@ -44,7 +53,7 @@ const ThikirAlarmScreen = () => {
     },
     notificationContainer: {
       backgroundColor: "#262626",
-      shadowColor: "black", 
+      shadowColor: "black",
     },
     title: {
       color: "#dddddd",
@@ -54,115 +63,152 @@ const ThikirAlarmScreen = () => {
     },
   });
   //#endregion
+
   const themeStyles = systemTheme
-    ? Appearance.getColorScheme() === 'dark'
+    ? Appearance.getColorScheme() === "dark"
       ? darkTheme
       : lightTheme
-    : selectedTheme === 'dark'
+    : selectedTheme === "dark"
     ? darkTheme
     : lightTheme;
-    //#region ArabicLanguage
-    const ArabicLanguage = StyleSheet.create({
-      notificationContainer: {
-        flexDirection: "row-reverse",
-      },
-      horizontalLineWrapper: {
-        marginLeft: width > 600 ? 610 : 350,
-      },
-      leftContent: {
-        alignItems: "flex-end",
-      },
-      rightContent: {
-        alignItems: "flex-start",
-        paddingLeft: 10,
-      },
-      title: {
-        textAlign: "right",
-        marginRight: 10,
-      },
-      time: {
-        marginRight: 10,
-        textAlign: "right",
-      },
-    });
-    //#endregion
-  
-    //#region EnglishLanguage
-    const EnglishLanguage = StyleSheet.create({
-      notificationContainer: {
-        flexDirection: "row",
-      },
-      horizontalLineWrapper: {
-        marginRight: width > 600 ? 610 : 350,
-      },
-      leftContent: {
-        alignItems: "flex-start",
-      },
-      rightContent: {
-        alignItems: "flex-end",
-        paddingRight: 10,
-      },
-      title: {
-        textAlign: "left",
-        marginLeft: 10,
-      },
-      time: {
-        marginLeft: 10,
-        textAlign: "left",
-      },
-    });
-    //#endregion
-  
+
+  //#region ArabicLanguage
+  const ArabicLanguage = StyleSheet.create({
+    notificationContainer: {
+      flexDirection: "row-reverse",
+    },
+    horizontalLineWrapper: {
+      marginLeft: width > 600 ? 610 : 350,
+    },
+    leftContent: {
+      alignItems: "flex-end",
+    },
+    rightContent: {
+      alignItems: "flex-start",
+      paddingLeft: 10,
+    },
+    title: {
+      textAlign: "right",
+      marginRight: 10,
+    },
+    time: {
+      marginRight: 10,
+      textAlign: "right",
+    },
+  });
+  //#endregion
+
+  //#region EnglishLanguage
+  const EnglishLanguage = StyleSheet.create({
+    notificationContainer: {
+      flexDirection: "row",
+    },
+    horizontalLineWrapper: {
+      marginRight: width > 600 ? 610 : 350,
+    },
+    leftContent: {
+      alignItems: "flex-start",
+    },
+    rightContent: {
+      alignItems: "flex-end",
+      paddingRight: 10,
+    },
+    title: {
+      textAlign: "left",
+      marginLeft: 10,
+    },
+    time: {
+      marginLeft: 10,
+      textAlign: "left",
+    },
+  });
+  //#endregion
+
   //#region StyleMapping
   const styles = {
     ...ThikirAlarmStyles,
     container: {
       ...ThikirAlarmStyles.container,
-      ...selectedTheme === 'dark' ? themeStyles.container : themeStyles.container,
+      ...(selectedTheme === "dark"
+        ? themeStyles.container
+        : themeStyles.container),
     },
     notificationContainer: {
       ...ThikirAlarmStyles.notificationContainer,
-      ...selectedTheme  === 'dark'? themeStyles.notificationContainer : themeStyles.notificationContainer,
-      ...(selectedLanguage != "Arabic" ? EnglishLanguage.notificationContainer : ArabicLanguage.notificationContainer )
+      ...(selectedTheme === "dark"
+        ? themeStyles.notificationContainer
+        : themeStyles.notificationContainer),
+      ...(selectedLanguage != "Arabic"
+        ? EnglishLanguage.notificationContainer
+        : ArabicLanguage.notificationContainer),
     },
     title: {
       ...ThikirAlarmStyles.title,
-      ...selectedTheme  === 'dark'? themeStyles.title : themeStyles.title,
-      ...(selectedLanguage != "Arabic" ? EnglishLanguage.title : ArabicLanguage.title )
+      ...(selectedTheme === "dark" ? themeStyles.title : themeStyles.title),
+      ...(selectedLanguage != "Arabic"
+        ? EnglishLanguage.title
+        : ArabicLanguage.title),
     },
     horizontalLineWrapper: {
       ...ThikirAlarmStyles.horizontalLineWrapper,
-      ...selectedTheme  === 'dark'? themeStyles.horizontalLineWrapper : themeStyles.horizontalLineWrapper,
-      ...(selectedLanguage != "Arabic" ? EnglishLanguage.horizontalLineWrapper : ArabicLanguage.horizontalLineWrapper )
+      ...(selectedTheme === "dark"
+        ? themeStyles.horizontalLineWrapper
+        : themeStyles.horizontalLineWrapper),
+      ...(selectedLanguage != "Arabic"
+        ? EnglishLanguage.horizontalLineWrapper
+        : ArabicLanguage.horizontalLineWrapper),
     },
-    leftContent:{
+    leftContent: {
       ...ThikirAlarmStyles.leftContent,
-      ...(selectedLanguage != "Arabic" ? EnglishLanguage.leftContent : ArabicLanguage.leftContent )
+      ...(selectedLanguage != "Arabic"
+        ? EnglishLanguage.leftContent
+        : ArabicLanguage.leftContent),
     },
-    rightContent:{
+    rightContent: {
       ...ThikirAlarmStyles.rightContent,
-      ...(selectedLanguage != "Arabic" ? EnglishLanguage.rightContent : ArabicLanguage.rightContent )
+      ...(selectedLanguage != "Arabic"
+        ? EnglishLanguage.rightContent
+        : ArabicLanguage.rightContent),
     },
-    time:{
+    time: {
       ...ThikirAlarmStyles.time,
-      ...(selectedLanguage != "Arabic" ? EnglishLanguage.time : ArabicLanguage.time )
+      ...(selectedLanguage != "Arabic"
+        ? EnglishLanguage.time
+        : ArabicLanguage.time),
     },
   };
   //#endregion
-  const thikirSleepTranslationTitle = selectedLanguage != "Arabic" ? "Sleeping Remembrance" : "أذكار النوم";
-  const thikirWakeupTranslationTitle = selectedLanguage != "Arabic" ? "Waking up Remembrance" :  "أذكار الاستيقاظ من النوم";
-  const thikirMorningTranslationTitle = selectedLanguage != "Arabic" ? "Morning Remembrance" :"أذكار الصباح";
-  const thikirEveningTranslationTitle = selectedLanguage != "Arabic" ? "Evening Remembrance" : "أذكار المساء";
 
+  //#region NotificationsTitlesAndBodyText
+  const thikirSleepTranslationTitle =
+    selectedLanguage != "Arabic" ? "Sleeping Remembrance" : "أذكار النوم";
+  const thikirWakeupTranslationTitle =
+    selectedLanguage != "Arabic"
+      ? "Waking up Remembrance"
+      : "أذكار الاستيقاظ من النوم";
+  const thikirMorningTranslationTitle =
+    selectedLanguage != "Arabic" ? "Morning Remembrance" : "أذكار الصباح";
+  const thikirEveningTranslationTitle =
+    selectedLanguage != "Arabic" ? "Evening Remembrance" : "أذكار المساء";
 
-  const thikirSleepTranslationBody = selectedLanguage != "Arabic" ? "In Your name my Lord, I lie down and in Your name I rise, so if You should take my soul then have mercy upon it, and if You should return my soul then protect it in the manner You do so with Your righteous servants." :
-    "بِاسْمِكَ رَبِّي وَضَعْتُ جَنْبِي، وَبِكَ أَرْفَعُهُ، فَإِن أَمْسَكْتَ نَفْسِي فارْحَمْهَا، وَإِنْ أَرْسَلْتَهَا فَاحْفَظْهَا، بِمَا تَحْفَظُ بِهِ عِبَادَكَ الصَّالِحِينَ";
-  const thikirWakeupTranslationBody = selectedLanguage != "Arabic" ? "All praise is for Allah who gave us life after having taken it from us and unto Him is the resurrection." :
-    "الْحَمْدُ للَّهِ الَّذِي أَحْيَانَا بَعْدَ مَا أَمَاتَنَا، وَإِلَيْهِ النُّشُورُ";
-  const thikirMorningTranslationBody = selectedLanguage != "Arabic" ? "O Allah, by your leave we have reached the morning and by Your leave we have reached the evening, by Your leave we live and die and unto You is our resurrection." :
-    "اللَّهُمَّ بِكَ أَصْبَحْنَا، وَبِكَ أَمْسَيْنَا ، وَبِكَ نَحْيَا، وَبِكَ نَمُوتُ وَإِلَيْكَ النُّشُورُ";
-  const thikirEveningTranslationBody = selectedLanguage != "Arabic" ? "O Allah, what blessing I or any of Your creation have risen upon, is from You alone, without partner, so for You is all praise and unto You all thanks." :
-    "اللَّهم بك أمسينا، وبك أصبحنا، وبك نحيا، وبك نموت، وإليك المصير";
+  const thikirSleepTranslationBody =
+    selectedLanguage != "Arabic"
+      ? "In Your name my Lord, I lie down and in Your name I rise, so if You should take my soul then have mercy upon it, and if You should return my soul then protect it in the manner You do so with Your righteous servants."
+      : "بِاسْمِكَ رَبِّي وَضَعْتُ جَنْبِي، وَبِكَ أَرْفَعُهُ، فَإِن أَمْسَكْتَ نَفْسِي فارْحَمْهَا، وَإِنْ أَرْسَلْتَهَا فَاحْفَظْهَا، بِمَا تَحْفَظُ بِهِ عِبَادَكَ الصَّالِحِينَ";
+  const thikirWakeupTranslationBody =
+    selectedLanguage != "Arabic"
+      ? "All praise is for Allah who gave us life after having taken it from us and unto Him is the resurrection."
+      : "الْحَمْدُ للَّهِ الَّذِي أَحْيَانَا بَعْدَ مَا أَمَاتَنَا، وَإِلَيْهِ النُّشُورُ";
+  const thikirMorningTranslationBody =
+    selectedLanguage != "Arabic"
+      ? "O Allah, by your leave we have reached the morning and by Your leave we have reached the evening, by Your leave we live and die and unto You is our resurrection."
+      : "اللَّهُمَّ بِكَ أَصْبَحْنَا، وَبِكَ أَمْسَيْنَا ، وَبِكَ نَحْيَا، وَبِكَ نَمُوتُ وَإِلَيْكَ النُّشُورُ";
+  const thikirEveningTranslationBody =
+    selectedLanguage != "Arabic"
+      ? "O Allah, what blessing I or any of Your creation have risen upon, is from You alone, without partner, so for You is all praise and unto You all thanks."
+      : "اللَّهم بك أمسينا، وبك أصبحنا، وبك نحيا، وبك نموت، وإليك المصير";
+  //#endregion
+
   //#region notification structure
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
   const [selectedNotification, setSelectedNotification] = useState(null);
@@ -175,13 +221,13 @@ const ThikirAlarmScreen = () => {
     },
     {
       title: thikirWakeupTranslationTitle,
-      body: thikirWakeupTranslationBody ,
+      body: thikirWakeupTranslationBody,
       id: "thikir_wakeup",
       isActive: true,
     },
     {
       title: thikirMorningTranslationTitle,
-      body: thikirMorningTranslationBody ,
+      body: thikirMorningTranslationBody,
       id: "thikir_morning",
       isActive: true,
     },
@@ -201,30 +247,30 @@ const ThikirAlarmScreen = () => {
 
   //#region getNotificationPermission
   const requestNotificationPermissions = async () => {
-    if (Platform.OS === 'android'){
+    if (Platform.OS === "android") {
       const { status } = await Notifications.requestPermissionsAsync();
       if (status !== "granted") {
-        const { status: newStatus } = await Notifications.requestPermissionsAsync();
-        if(newStatus ==='granted'){
+        const { status: newStatus } =
+          await Notifications.requestPermissionsAsync();
+        if (newStatus === "granted") {
           console.log("Notification permissions granted");
         }
       } else {
         console.log("Notification permissions denied");
       }
-    }else{
-      const { status } = await Notifications.requestPermissionsAsync();
-    if (status === "granted") {
-      console.log("Notification permissions granted");
     } else {
-      console.log("Notification permissions denied");
-    }
+      const { status } = await Notifications.requestPermissionsAsync();
+      if (status === "granted") {
+        console.log("Notification permissions granted");
+      } else {
+        console.log("Notification permissions denied");
+      }
     }
   };
   //#endregion
 
   //#region toggleAlarm
   const toggleAlarm = async (notificationId, newValue) => {
-
     const updatedNotifications = notifications.map((notification) =>
       notification.id === notificationId
         ? { ...notification, isActive: newValue }
@@ -278,20 +324,19 @@ const ThikirAlarmScreen = () => {
     console.log(`Notification for ${notificationId} scheduled.`);
   };
   //#endregion
-  
-  //#region display datapicker
+
+  //#region display datepicker
   const showDatePicker = (notification) => {
     setSelectedNotification(notification);
     setDatePickerVisibility(true);
   };
-
 
   const hideDatePicker = () => {
     setDatePickerVisibility(false);
   };
 
   //#endregion
-  
+
   //#region handleConfirmation
   const handleConfirm = async (date) => {
     hideDatePicker();
@@ -314,8 +359,8 @@ const ThikirAlarmScreen = () => {
   };
 
   //#endregion
-  
-  //#region loadStatesOfalarmTimeAndNotifications
+
+  //#region loadStatesAlarmTimeAndNotifications
   const loadNotificationStatesAndAlarmTimes = async () => {
     try {
       const storedDataJSON = await AsyncStorage.getItem("notificationData");
@@ -342,14 +387,14 @@ const ThikirAlarmScreen = () => {
   };
   //#endregion
 
-  //#region handleCancelNotifaction
+  //#region handleCancelNotification
   const cancelNotification = async (notificationId) => {
     await Notifications.cancelScheduledNotificationAsync(notificationId);
     console.log(`Notification for ${notificationId} canceled.`);
   };
   //#endregion
-    
-  //#region handleSaveNotifactiondData
+
+  //#region handleSaveNotificationData
   const saveNotificationData = async (notificationsToSave) => {
     try {
       const notificationData = {};
@@ -369,8 +414,8 @@ const ThikirAlarmScreen = () => {
     }
   };
   //#endregion
-  
-  //#region DispalyBorderRadiusBasedOnItemCount
+
+  //#region DisplayBorderRadiusBasedOnItemCount
   const renderBorderRadius = (index) => {
     const itemCount = notifications.length;
     if (index === 0) {
@@ -381,7 +426,7 @@ const ThikirAlarmScreen = () => {
     } else if (index === itemCount - 1) {
       return { borderBottomLeftRadius: 10, borderBottomRightRadius: 10 };
     }
-    return {}; 
+    return {};
   };
   //#endregion
 
@@ -393,19 +438,40 @@ const ThikirAlarmScreen = () => {
             onPress={() => showDatePicker(notification)}
             activeOpacity={0.7}
           >
-            <View style={[styles.notificationContainer, renderBorderRadius(index),]}>
+            <View
+              style={[styles.notificationContainer, renderBorderRadius(index)]}
+            >
               <View style={styles.leftContent}>
-                <Text   numberOfLines={1}
-                        ellipsizeMode="tail" 
-                        allowFontScaling={false} style={[styles.title,{fontFamily: selectedLanguage!= "Arabic" ? "Montserrat" : "ScheherazadeNewBold"}]}>{notification.title}</Text>
+                <Text
+                  numberOfLines={1}
+                  ellipsizeMode="tail"
+                  allowFontScaling={false}
+                  style={[
+                    styles.title,
+                    {
+                      fontFamily:
+                        selectedLanguage != "Arabic"
+                          ? "Montserrat"
+                          : "ScheherazadeNewBold",
+                    },
+                  ]}
+                >
+                  {notification.title}
+                </Text>
                 <Text allowFontScaling={false} style={styles.time}>
-                    {TimeToDisplay = state.isArabicNumbers
-                    ? convertToEasternArabicNumerals(notification.date
-                    ? formatTime(notification.date)
-                    : formatTime(getDefaultTime(notification.id)).toString())
-                    : notification.date
-                    ? formatTime(notification.date)
-                    : formatTime(getDefaultTime(notification.id)).toString()}
+                  {
+                    (TimeToDisplay = state.isArabicNumbers
+                      ? convertToEasternArabicNumerals(
+                          notification.date
+                            ? formatTime(notification.date)
+                            : formatTime(
+                                getDefaultTime(notification.id)
+                              ).toString()
+                        )
+                      : notification.date
+                      ? formatTime(notification.date)
+                      : formatTime(getDefaultTime(notification.id)).toString())
+                  }
                 </Text>
               </View>
               {/* <View style={styles.middleContent}></View> */}
@@ -415,8 +481,8 @@ const ThikirAlarmScreen = () => {
                   onValueChange={() =>
                     toggleAlarm(notification.id, !notification.isActive)
                   }
-                  thumbColor={notification.isActive ? '#fefffe' : '#fefffe'}
-                  trackColor={{ true: selectedColor, false: '#454545' }}
+                  thumbColor={notification.isActive ? "#fefffe" : "#fefffe"}
+                  trackColor={{ true: selectedColor, false: "#454545" }}
                 />
               </View>
             </View>
@@ -432,7 +498,7 @@ const ThikirAlarmScreen = () => {
         date={
           selectedNotification
             ? selectedNotification.date ||
-            getDefaultTime(selectedNotification.id)
+              getDefaultTime(selectedNotification.id)
             : new Date()
         }
         onConfirm={handleConfirm}
@@ -448,7 +514,7 @@ const formatTime = (time) => {
 };
 //#endregion
 
-//#region GetDefaulTimeOfNotifications
+//#region GetDefaultTimeOfNotifications
 const getDefaultTime = (notificationId) => {
   // Define default times for each notification based on their IDs
   const defaultTimes = {
