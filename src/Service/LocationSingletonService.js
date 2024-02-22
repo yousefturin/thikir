22426ce7834d947be76bd59e136168e76cd97aa1
yousefturin <yusefturin@gmail.com>
2024-02-22 16:18:59 +0300

    issue with getting location resolved

--- src/Service/LocationSingletonService.js
+++ src/Service/LocationSingletonService.js
@@ -1,27 +1,25 @@
-// locationSingleton.js
 import * as Location from "expo-location";
 
 const locationSingleton = {
     location: null,
     async getLocation() {
-        // console.log("=============================================")
-        // console.log("I am starting the Singleton case for Location");
-        
-        if (!this.location) {
-            try {
-                const { status } = await Location.requestForegroundPermissionsAsync();
-                if (status !== "granted") {
-                    return { locationSuccess: false, error: "Permission denied" };
-                }
-                const { coords } = await Location.getCurrentPositionAsync({});
+        try {
+            const { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== "granted") {
+                return { locationSuccess: false, error: "Permission denied" };
+            }
+            const { coords } = await Location.getCurrentPositionAsync({});
+            // Compare with cached location
+            if (this.location && this.location.latitude === coords.latitude && this.location.longitude === coords.longitude) {
+                return { locationSuccess: true, location: this.location };
+            } else {
                 this.location = coords;
                 return { locationSuccess: true, location: this.location };
-            } catch (error) {
-                console.error("Error getting location", error);
-                return { locationSuccess: false, error };
             }
+        } catch (error) {
+            console.error("Error getting location", error);
+            return { locationSuccess: false, error };
         }
-        return { locationSuccess: true, location: this.location };
     },
 };
 
