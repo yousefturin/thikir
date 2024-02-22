33861006cf78248812f4e888ef364fc725d7b2be
yousefturin <yusefturin@gmail.com>
2024-02-22 14:04:47 +0300

    sequential execution is added

--- src/Service/LocationSingletonService.js
+++ src/Service/LocationSingletonService.js
@@ -4,24 +4,24 @@ import * as Location from "expo-location";
 const locationSingleton = {
     location: null,
     async getLocation() {
-        console.log("=============================================")
-        console.log("I am starting the Singleton case for Location");
+        // console.log("=============================================")
+        // console.log("I am starting the Singleton case for Location");
+        
         if (!this.location) {
             try {
                 const { status } = await Location.requestForegroundPermissionsAsync();
                 if (status !== "granted") {
-                    console.log("Permission to access location was denied");
-                    return null; // Return null if permission is not granted
+                    return { locationSuccess: false, error: "Permission denied" };
                 }
-                console.log("Exiting Singleton case for Location ");
                 const { coords } = await Location.getCurrentPositionAsync({});
                 this.location = coords;
+                return { locationSuccess: true, location: this.location };
             } catch (error) {
                 console.error("Error getting location", error);
-                return null; // Return null if there's an error
+                return { locationSuccess: false, error };
             }
         }
-        return this.location;
+        return { locationSuccess: true, location: this.location };
     },
 };
 
