a5c3448ee514b99c665b5d8ba8e260276dce2612
yousefturin <yusefturin@gmail.com>
2024-02-26 12:43:17 +0300

    under testing, to check errors from location reading

--- src/Service/LocationSingletonService.js
+++ src/Service/LocationSingletonService.js
@@ -6,20 +6,26 @@ const locationSingleton = {
         try {
             const { status } = await Location.requestForegroundPermissionsAsync();
             if (status !== "granted") {
-                return { locationSuccess: false, error: "Permission denied" };
+                return { locationSuccess: false, locationMatch: null, error: "Permission denied" };
             }
             const { coords } = await Location.getCurrentPositionAsync({});
             // Compare with cached location
+
             if (this.location && this.location.latitude === coords.latitude && this.location.longitude === coords.longitude) {
-                return { locationSuccess: true, location: this.location };
+                return { locationSuccess: true, locationMatch: true, location: this.location };
             } else {
                 this.location = coords;
-                return { locationSuccess: true, location: this.location };
+                console.log(this.location)
+                return { locationSuccess: true, locationMatch: false, location: this.location };
             }
+
         } catch (error) {
             console.error("Error getting location", error);
-            return { locationSuccess: false, error };
+            return { locationSuccess: false, locationMatch: null, error };
         }
+
+
+
     },
 };
 
