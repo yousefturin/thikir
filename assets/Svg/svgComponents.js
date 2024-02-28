46cd14a37380c4cfeb21d00f8db6c639016b328b
yousefturin <yusefturin@gmail.com>
2024-02-28 17:26:46 +0300

    fill is now passed using the component

--- assets/Svg/svgComponents.js
+++ assets/Svg/svgComponents.js
@@ -2,8 +2,8 @@ import React from "react";
 import { SvgXml } from "react-native-svg";
 import svgData from "./SvgStorage";
 
-export default function SvgComponent({ svgKey }) {
+export default function SvgComponent({ svgKey,fill }) {
     const svgMarkup = svgData[svgKey];
 
-    return <SvgXml xml={svgMarkup} width="301px" />;
+    return <SvgXml xml={svgMarkup} width="301px" fill={fill} />;
 }
