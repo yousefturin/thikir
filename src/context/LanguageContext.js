9d34afe66b4d8fe37e43a20f81de453060ae1416
yousefturin <yusefturin@gmail.com>
2024-03-02 23:52:28 +0300

    loading icon is adapted to device screen

--- src/context/LanguageContext.js
+++ src/context/LanguageContext.js
@@ -1,12 +1,15 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { View, ActivityIndicator, Image} from 'react-native';
+import { View, ActivityIndicator, Image, Dimensions } from 'react-native';
+import initializeScalingUtils from "../utils/core/NormalizeSize";
+
 
 
 const LanguageContext = createContext();
 
 export function LanguageProvider({ children }) {
     const [selectedLanguage, setSelectedLanguage] = useState("Arabic"); // Default font selection
+    const { moderateScale } = initializeScalingUtils(Dimensions);
     const [loading, setLoading] = useState(true);
     useEffect(() => {
         // Load the selected font from AsyncStorage on app startup
@@ -18,16 +21,16 @@ export function LanguageProvider({ children }) {
                 }
             } catch (error) {
                 console.error("Error fetching Language:", error);
-            }finally {
+            } finally {
                 setTimeout(() => {
-                setLoading(false); // Mark loading as complete
-            }, 300); 
+                    setLoading(false); // Mark loading as complete
+                }, 300);
             }
         }
 
         fetchLanguage();
     }, []);
-    const appIconImage = require("../../assets/Images/splashEntro.png");
+    const appIconImage = require("../../assets/Images/splashScreen.png");
     // Set the selected font
     const setLanguage = (Language) => {
         setSelectedLanguage(Language);
@@ -45,16 +48,16 @@ export function LanguageProvider({ children }) {
     if (loading) {
         // Return a loading indicator while the language is being loaded
         return (
-            <View style={{ flex: 1, backgroundColor: "#151515" ,paddingTop:240}}>
-            <View style={{ flex: 1, justifyContent:"flex-start", alignItems: 'center'  }}>
-                <Image
-                source={appIconImage}
-                style={{resizeMode: "cover", width: 420, height: 420 }}
-                />
-            </View>
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <ActivityIndicator size="small" color="#242424" style={{ width: "100%", height: 20 }} />
-            </View>
+            <View style={{ flex: 1, backgroundColor: "#151515", paddingTop: moderateScale(200) }}>
+                <View style={{ flex: 1, justifyContent: "flex-start", alignItems: 'center' }}>
+                    <Image
+                        source={appIconImage}
+                        style={{ resizeMode: "cover", width: moderateScale(380), height:moderateScale(380) }}
+                    />
+                </View>
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                    <ActivityIndicator size="small" color="#5c5b5b" style={{ width: "100%", height: 20 }} />
+                </View>
             </View>
         );
     }
